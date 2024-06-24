c67bfa40b5f988908ede88774286230216c158c5
KalaburigiPoojitha <kalaburigipoojitha05@gmail.com>
2024-06-24 14:17:26 +0530

    Add files via upload

---
+++ intro/src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Galaxy S24 Ultra</h1>
+        <h2>Galaxy AI is here</h2>
+        <img src="https://images.samsung.com/in/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-highlights-kv.jpg?imbypass=true"></img>
+      </div>
+      <div className="box">
+        <h5>Camera </h5>
+        <p>"For those who want the image adjustment with AI"</p>
+        <h5>Forbes Vetted</h5>
+      </div>
+      <div className="box1">
+        <h5>Galaxy AI</h5>
+        <p>"The new era of AI-enhanced smartphones"</p>
+        <h5>GQ</h5>
+      </div>
+      <div className="box2">
+        <h5>Galaxy AI</h5>
+        <p>"Galaxy AI actually feels like the future of phones"</p>
+        <h5>Inverse</h5>
+      </div>
+      <div>
+        <h1 id="h">Sleek.Strong.Stunning</h1>
+      </div>
+      <div className="image">
+        <img src="https://images.samsung.com/in/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-highlights-titanium.jpg?imbypass=true"></img>
+        <div className="image1">
+          <h2>Armor up with titanium</h2>
+          <p>
+            Check out the upgrade — a durable shield of titanium built right
+            into the frame.8 The strength of titanium is now at your fingertips.
+          </p>
+          <p>
+            Meet Galaxy S24 Ultra, the ultimate form of Galaxy Ultra with a new
+            titanium exterior and a 17.25cm flat display.7,8 It's an absolute
+            marvel of design.
+          </p>
+        </div>
+      </div>
+      <div className="h3">
+        <h3>New colours. </h3>
+        <h3>Mineral chic10</h3>
+        <img src="https://images.samsung.com/in/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-highlights-color-carousel-exclusive.jpg?imbypass=true"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
